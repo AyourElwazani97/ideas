b71ebcac7180bff5b5e6b722f9fd2ae332e7fffd
ayourelwazani97 <ayoubwazane306@gmail.com>
2024-01-14 15:10:16 +0100

    adding new controller and updating form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IdeasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/dashboard', [DashboardController::class, "index"]);
+Route::resource("ideas", IdeasController::class);
 Route::get("/terms", function () {
     return view("terms");
 });
