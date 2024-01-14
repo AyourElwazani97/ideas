c6ea49dbf5101719b8631599001134f5c3172be3
ayourelwazani97 <ayoubwazane306@gmail.com>
2024-01-14 19:32:07 +0100

    updating routes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{config("app.name")}}</title>
+    <title>{{ config('app.name') }}</title>
 
     <link href="https://bootswatch.com/5/sketchy/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
@@ -18,7 +18,8 @@
     <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark ticky-top bg-body-tertiary"
         data-bs-theme="dark">
         <div class="container">
-            <a class="navbar-brand fw-light" href="/"><span class="fas fa-brain me-1"> </span>Ideas</a>
+            <a class="navbar-brand fw-light" href="{{ route('ideas.index') }}"><span class="fas fa-brain me-1">
+                </span>Ideas</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 /* Route::get('/dashboard', [DashboardController::class, "index"]); */
 /* Route::get("/terms", function () {
     return view("terms");
-}); */
+}); 
+Route::post('/blabla/{id}',[ourClass::class,'destroy']->name(index.destroy))
+*/
 
 Route::resource("ideas", IdeasController::class);
